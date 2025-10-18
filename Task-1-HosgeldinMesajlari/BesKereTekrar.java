5d98a370b117f09c23417a6f189fcad051cd685c
Alaa <alaamadi005@gmail.com>
2025-10-18 09:08:23 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,21 @@
+public class Main{
+  public static void main(String[] args){
+       for ( int i = 0; i < 5; i++){
+          System.out.println("welcom to the java class");
+      }
+  }
+}  
+
+
+// baska be sekilde yazabiliriz sadece println kullanmaya //
+
+public class Main{
+  public static void main(String[] args) {
+    System.out.println("welcom to the java world");
+    System.out.println("welcom to the java world");
+    System.out.println("welcom to the java world");
+    System.out.println("welcom to the java world");
+    System.out.println("welcom to the java world");
+  }
+}
+  
