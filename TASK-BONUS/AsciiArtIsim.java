9c3a6686e28767057e0e38488586e38a477ecfeb
Alaa <240541609@firat.edu.tr>
2025-10-18 11:06:03 +0300

    Add AsciiArtIsim class for ASCII art representation

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,10 @@
+public class AsciiArtIsim {
+    public static void main(String[] args) {
+        System.out.println("    A       L       A       A       ");
+        System.out.println("   A A      L      A A     A A      ");
+        System.out.println("  A   A     L     A   A   A   A     ");
+        System.out.println("  AAAAA     L     AAAAA   AAAAA     ");
+        System.out.println("  A   A     L     A   A   A   A     ");
+        System.out.println("  A   A     L     A   A   A   A     ");
+    }
+}
