2949343b8c004e936bc8458bf8d8e5d35e284780
Alaa <240541609@firat.edu.tr>
2025-10-18 09:27:19 +0300

    Add welcome messages in UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class ucMasej{
+  public static void main(String[] args) {
+    System.out.println(" welcom to java world");
+    System.out.println("programing with java is sooo fun");
+    System.out.println("start learning java now!");
+  }
+}
