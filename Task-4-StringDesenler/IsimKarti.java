8dced429f23ab09811fdc208d0f65b6215ef0894
Alaa <240541609@firat.edu.tr>
2025-10-18 10:48:29 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Alaa";
+        String soyad = "Madi";
+        String ogrenciNo = "240541609";
+        String bolum = "yazilim Mühendisliği";
+
+        System.out.println("******************************");
+        System.out.println("*      ogrenci bilgileri     *");
+        System.out.println("******************************");
+        System.out.println("* Ad          : " + ad);
+        System.out.println("* Soyad       : " + soyad);
+        System.out.println("* Öğrenci No  : " + ogrenciNo);
+        System.out.println("* Bölüm       : " + bolum);
+        System.out.println("******************************");
+    }
+}
