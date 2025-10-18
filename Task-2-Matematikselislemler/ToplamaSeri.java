c8e270b5ea7cbd47631f6f0c602003d1da3ce4cd
Alaa <240541609@firat.edu.tr>
2025-10-18 11:39:00 +0300

    Update ToplamaSeri.java

--- Task-2-Matematikselislemler/ToplamaSeri.java
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -1,5 +1,5 @@
 // doning this using println //
-public class Calculate{
+public class ToplamaSeri{
   public static void main(String[] args){
     int toplam = 1+2+3+4+5+6+7+8+9;
     System.out.println("toplam =" + toplam);
@@ -7,9 +7,10 @@ public class Calculate{
 }
 
 // daha kolay bir sekilde yapabilirim//
-public class Calculate{
+public class ToplamaSeri{
   public static void main(String[] args){
     int result = 0
     for(int i = 1; i <= 9; i++){
       int result += i
       System.out.println("toplam = "+ result);
+
