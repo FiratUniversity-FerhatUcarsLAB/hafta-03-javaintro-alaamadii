5ecbf9b0b416d6bd67fe360c875742c5e5f35b80
Alaa <240541609@firat.edu.tr>
2025-10-18 09:55:11 +0300

    Update IkiIslem.java

---
+++ Task-2-Matematikselislemler/IkiIslem.java
@@ -0,0 +1,8 @@
+public class Calculate{
+  public static void main(String[] args){
+    double pay = (9.5*4.5)-(2.5*3);
+    double payda = 45.5 - 3.5;
+    double sounuc = pay/payda;
+    System.out.println("sonucu = %.4f\n ", sounuc);
+  }
+}
