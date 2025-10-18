d9d2d1c033d5378d63a26bc3766c847b7a2dd6c2
Alaa <240541609@firat.edu.tr>
2025-10-18 10:13:51 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,14 @@
+public class Daire{
+  public static void main(String[] args){
+    double r = 5.5;
+    double pi = 3.1415;
+    // Alan hesape //
+    double alan = pi * r * r;
+    // cevre hesaplan//
+    double cever = 2 * pi * r;
+
+    System.out.println("yaricap = " + r);
+    System.out.println("Alan = " + alan);
+    System.out.println("cevre = " + cever);
+  }
+}
