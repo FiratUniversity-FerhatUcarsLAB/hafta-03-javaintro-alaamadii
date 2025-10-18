23603484429a387a42f8c7b19881d61b103c489d
Alaa <240541609@firat.edu.tr>
2025-10-18 10:25:00 +0300

    Update DikdortgenHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DikdortgenHesap.java
@@ -0,0 +1,16 @@
+public class Dikdortgen {
+    public static void main(String[] args) {
+        double en = 4.5;
+        double boy = 7.9;
+
+
+        double alan = en * boy;
+
+    
+        double cevre = 2 * (en + boy);
+
+    
+        System.out.println("Dikdortgen boy = " + boy + " ve en = " + en + ", cevre = " + cevre);
+        System.out.println("Ve onun alan = " + alan);
+    }
+}
